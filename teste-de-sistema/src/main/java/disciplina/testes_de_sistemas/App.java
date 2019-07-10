30f50d5189e759a8a5c1e83bacf179bbd36e06f6
Lazaro Santos <lazaro.santos@resourceit.com>
2019-07-10 17:00:13 -0300

    Projeto da disciplina de testes de sistemas

---
+++ teste-de-sistema/src/main/java/disciplina/testes_de_sistemas/App.java
@@ -0,0 +1,13 @@
+package disciplina.testes_de_sistemas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
